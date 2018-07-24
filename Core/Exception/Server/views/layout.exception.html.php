689e17a2688e40b71a4d3d829c2dddc99ee16bc8
Dany Rafina <danyrafina@gmail.com>
2018-07-24 19:56:20 +0200

    - Bug fixes v2

--- Core/Exception/Server/views/layout.exception.html.php
+++ Core/Exception/Server/views/layout.exception.html.php
@@ -2,37 +2,38 @@
 <html lang="en">
 <head>
     <meta charset="utf-8" />
-    <link rel="icon" type="image/png" href="<?= \iumioFramework\Core\Requirement\Environment\FEnv::get("host.web.components.libs.framework") ?>assets/images/favicon.ico/favicon.ico">
-    <title><?= $this->code.' '.strtolower(ucfirst($this->codeTitle)).' - Environment '.(ucfirst(strtolower($this->env))) ?></title>
+    <link rel="icon" type="image/png" href="<?= $libf ?>assets/images/favicon.ico/favicon.ico">
+    <title><?= $title ?></title>
 
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
 
 
     <!-- Bootstrap core CSS     -->
-    <link href="<?= \iumioFramework\Core\Requirement\Environment\FEnv::get("host.web.components.libs").'bootstrap/' ?>css/bootstrap.min.css" rel="stylesheet" />
+    <link href="<?= $boot ?>css/bootstrap.min.css" rel="stylesheet" />
 
     <!-- Animation library for notifications   -->
-    <link href="<?= \iumioFramework\Core\Requirement\Environment\FEnv::get("host.web.components.libs").'animate.css/' ?>animate.min.css" rel="stylesheet"/>
+    <link href="<?= $anim ?>animate.min.css" rel="stylesheet"/>
 
     <!--  Light Bootstrap Table core CSS    -->
-    <link href="<?= \iumioFramework\Core\Requirement\Environment\FEnv::get("host.web.components.libs").'iumio-manager/' ?>css/light-bootstrap-dashboard.css" rel="stylesheet"/>
+    <link href="<?= $im ?>css/light-bootstrap-dashboard.css" rel="stylesheet"/>
 
 
-    <link href="<?= \iumioFramework\Core\Requirement\Environment\FEnv::get("host.web.components.libs").'iumio-manager/' ?>css/index.css" rel="stylesheet" />
+    <link href="<?= $im ?>css/index.css" rel="stylesheet" />
     <!--  CSS for Demo Purpose, don't include it in your project     -->
-    <link href="<?= \iumioFramework\Core\Requirement\Environment\FEnv::get("host.web.components.libs").'iumio-manager/' ?>css/demo.css" rel="stylesheet" />
+    <link href="<?= $im ?>css/demo.css" rel="stylesheet" />
 
 
     <!--     Fonts and icons     -->
-    <link href="<?= \iumioFramework\Core\Requirement\Environment\FEnv::get("host.web.components.libs").'font-awesome/' ?>css/font-awesome.min.css" rel="stylesheet">
-    <link href="<?= \iumioFramework\Core\Requirement\Environment\FEnv::get("host.web.components.libs").'/iumio-manager/' ?>css/pe-icon-7-stroke.css" rel="stylesheet" />
+    <link href="<?= $font ?>css/font-awesome.min.css" rel="stylesheet">
+    <link href="<?= $im ?>css/pe-icon-7-stroke.css" rel="stylesheet" />
 
 </head>
 <body style="background: rgba(203, 203, 210, 0.15);">
-<div class="se-pre-con"> <h3 class='validform'><?= 'An exception was generated - Environment '.(strtoupper(strtolower($this->env))) ?></h3> </div>
+<div class="se-pre-con"> <h3 class='validform'><?= $msg ?></h3> </div>
 <div class="wrapper">
-    <nav class="navbar navbar-default navbar-fixed fgm-black" style="font-size: 14px;color: white;min-height: 0px;margin-bottom: 0px">
+    <nav class="navbar navbar-default navbar-fixed fgm-black"
+         style="font-size: 14px;color: white;min-height: 0px;margin-bottom: 0px">
         <div class="container">
             <div class="col-md-6">
                 <a href="#"><i class="fa fa-pagelines"></i> <strong>Framework exception page</strong></a>
@@ -56,7 +57,7 @@
                 </div>
             </div>
             <div class="col-md-4" style="text-align: center;">
-                <img src="<?= \iumioFramework\Core\Requirement\Environment\FEnv::get("host.web.components.libs.framework").'img/' ?>iumio.logo.white.framework.png" width="140" style="padding-top: 40px"/>
+                <img src="<?= $fimg ?>iumio.logo.white.framework.png" width="140" style="padding-top: 40px"/>
             </div>
         </div>
     </nav>
@@ -72,22 +73,33 @@
                                 <h4 class="title text-center">Characteristics</h4>
                                 <p></p>
                                 <hr>
-                                <p class="category fs16"><i class="pe-7s-config"></i> Uidie : <strong><?= $this->uidie ?></strong></p>
+                                <p class="category fs16"><i class="pe-7s-config"></i>
+                                    Uidie : <strong><?= $this->uidie ?></strong></p>
                                 <hr>
-                                <p class="category fs16"><i class="pe-7s-target"></i> Event code : <?= $this->code ?> <?= $this->codeTitle ?></p>
+                                <p class="category fs16"><i class="pe-7s-target"></i>
+                                    Event code : <?= $this->code ?> <?= $this->codeTitle ?></p>
                                 <hr>
-                                <p class="category fs16"><i class="pe-7s-clock"></i> Date : <?php echo (new \DateTime($this->time))->format("d/m/Y").
+                                <p class="category fs16">
+                                    <i class="pe-7s-clock"></i>
+                                    Date : <?php echo (new \DateTime($this->time))->format("d/m/Y").
                                         ' at '.(new \DateTime($this->time))->format("H:i:s") ?></p>
                                 <hr>
-                                <p class="category fs16"><i class="pe-7s-magnet"></i> Method : <?php echo  $_SERVER['REQUEST_METHOD'] ?></p>
+                                <p class="category fs16">
+                                    <i class="pe-7s-magnet"></i> Method : <?php echo  $_SERVER['REQUEST_METHOD'] ?></p>
                                 <hr>
-                                <p class="category fs16"><i class="pe-7s-server"></i> Environment : <?= ucfirst(strtolower($this->env)) ?></p>
+                                <p class="category fs16">
+                                    <i class="pe-7s-server"></i> Environment : <?= ucfirst(strtolower($this->env)) ?>
+                                </p>
                                 <hr>
-                                <p class="category fs16"><i class="pe-7s-link"></i> Path : <?= $_SERVER['REQUEST_URI'] ?></p>
+                                <p class="category fs16">
+                                    <i class="pe-7s-link"></i> Path : <?= $_SERVER['REQUEST_URI'] ?></p>
                                 <hr>
-                                <p class="category fs16"><i class="pe-7s-user"></i> Referer IP : { <?php echo  $this->client_ip ?>}</p>
+                                <p class="category fs16">
+                                    <i class="pe-7s-user"></i> Referer IP : { <?php echo  $this->client_ip ?>}</p>
                                 <hr>
-                                <?= (($this->type_error != null)? '<p class="category fs16"><i class="pe-7s-close-circle"></i> Type : '.$this->type_error.'</p><hr/>' : "") ?>
+                                <?= (($this->type_error != null)?
+                                    '<p class="category fs16"><i class="pe-7s-close-circle"></i> Type : '.
+                                    $this->type_error.'</p><hr/>' : "") ?>
                             </div>
                             <div class="content text-center">
                             </div>
@@ -101,20 +113,27 @@
                             </div>
                             <div class="content" style="padding-top: 0px">
                                 <hr>
-                                <p class="category fs16"><i class="pe-7s-info"></i> Message :  <span class="fw900 break-word"><?= $this->explain ?></span></h5></p>
+                                <p class="category fs16"><i class="pe-7s-info"></i>
+                                    Message :  <span class="fw900 break-word"><?= $this->explain ?></span></h5></p>
                                 <hr>
                                 <?php if ($this->file_error != null) { ?>
-                                    <p class="category fs16 "><i class="pe-7s-paperclip"></i> File :  <span class="fs16 filelink "><?= $this->file_error ?></span></p>
+                                    <p class="category fs16 ">
+                                        <i class="pe-7s-paperclip"></i> File :  <span class="fs16 filelink ">
+                                            <?= $this->file_error ?></span></p>
 
                                     <hr>
                                 <?php } ?>
 
                                 <?php if ($this->line_error != null) { ?>
-                                    <p class="category fs16 "><i class="pe-7s-target"></i> Line :  <span class="fw900"><?= $this->line_error ?></span></p>
+                                    <p class="category fs16 ">
+                                        <i class="pe-7s-target"></i> Line :  <span class="fw900">
+                                            <?= $this->line_error ?></span></p>
                                     <hr>
                                 <?php } ?>
 
-                                <p class="category fs16 "><i class="pe-7s-magic-wand"></i> Solution :  <span class=""><?= $this->solution ?></span></p>
+                                <p class="category fs16 ">
+                                    <i class="pe-7s-magic-wand"></i> Solution :  <span class="">
+                                        <?= $this->solution ?></span></p>
                                 <hr>
                             </div>
                         </div>
@@ -162,7 +181,11 @@
                                         <?php } ?>
                                         <div class="typo-line">
                                         <span class="break-word"><p class="category">Function
-                                                <?= (isset($one['line']))? "& Line" : "" ?></p><?= (($one['class'] ?? '')).($one['type'] ?? ''). $one['function'] ?> <?= (isset($one['line']))? "on line ". $one['line'] : "" ?></span>
+                                                <?= (isset($one['line']))? "& Line" : "" ?></p>
+                                            <?= (($one['class'] ?? '')).($one['type'] ?? '').
+                                            $one['function'] ?> <?= (isset($one['line']))? "on line ".
+                                                $one['line'] : "" ?>
+                                        </span>
                                         </div>
                                     </div>
                                     <hr>
@@ -189,17 +212,17 @@
 </body>
 
 <!--   Core JS Files   -->
-<script src="<?= \iumioFramework\Core\Requirement\Environment\FEnv::get("host.web.components.libs").'jquery/' ?>jquery.js" type="text/javascript"></script>
-<script src="<?= \iumioFramework\Core\Requirement\Environment\FEnv::get("host.web.components.libs").'bootstrap/' ?>js/bootstrap.min.js" type="text/javascript"></script>
+<script src="<?= $libs.'jquery/' ?>jquery.js" type="text/javascript"></script>
+<script src="<?= $libs.'bootstrap/' ?>js/bootstrap.min.js" type="text/javascript"></script>
 
 <!--  Checkbox, Radio & Switch Plugins -->
-<script src="<?= \iumioFramework\Core\Requirement\Environment\FEnv::get("host.web.components.libs").'iumio-manager/' ?>js/bootstrap-checkbox-radio-switch.js"></script>
+<script src="<?= $libs.'iumio-manager/' ?>js/bootstrap-checkbox-radio-switch.js"></script>
 
 <!--  Notifications Plugin    -->
-<script src="<?= \iumioFramework\Core\Requirement\Environment\FEnv::get("host.web.components.libs").'iumio-manager/' ?>js/bootstrap-notify.js"></script>
+<script src="<?= $libs.'iumio-manager/' ?>js/bootstrap-notify.js"></script>
 
-<script src="<?= \iumioFramework\Core\Requirement\Environment\FEnv::get("host.web.components.libs").'iumio-manager/' ?>js/demo.js"></script>
+<script src="<?= $libs.'iumio-manager/' ?>js/demo.js"></script>
 
-<script src="<?= \iumioFramework\Core\Requirement\Environment\FEnv::get("host.web.components.libs").'iumio-manager/' ?>js/main.js"></script>
+<script src="<?= $libs.'iumio-manager/' ?>js/main.js"></script>
 
 </html>
